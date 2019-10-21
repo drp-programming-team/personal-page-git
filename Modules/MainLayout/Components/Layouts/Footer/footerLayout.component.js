0cfa9603efe785c3e55fb1eeab81bd379e607c67
masoomeh <masoomeh.attaei@gmail.com>
2019-10-21 13:26:54 +0330

    Change Footer Component

--- Modules/MainLayout/Components/Layouts/Footer/footerLayout.component.js
+++ Modules/MainLayout/Components/Layouts/Footer/footerLayout.component.js
@@ -1,6 +1,5 @@
 mainLayoutApp.component("footerLayout", {
     templateUrl: "Modules/MainLayout/Components/Layouts/Footer/footerLayout.component.html",
     controller: function($scope) {
-        alert("Start footerLayout");
     }
 });
