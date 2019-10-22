b33bc4919a870d41641de1fd02c414691b1a8472
zahra <zahra.sbktkn@gmail.com>
2019-10-22 12:12:23 +0330

    complate menu design

--- Modules/MainLayout/Components/Layouts/Menu/menuLayout.component.js
+++ Modules/MainLayout/Components/Layouts/Menu/menuLayout.component.js
@@ -1,6 +1,17 @@
 mainLayoutApp.component("menuLayout", {
     templateUrl: "Modules/MainLayout/Components/Layouts/Menu/menuLayout.component.html",
     controller: function($scope) {
+        $scope.goSection=function(type) {
+         
+            $(".navbar-nav li").removeClass("active");
+            $('.' + type).addClass('active');
+                $('html, body').animate({
+                    scrollTop: $('#' + type).offset().top 
+                }, 'slow');
+        }
 
+        $('body').click(function () { 
+            $(".navbar-collapse").removeClass("in");    
+        });
     }
 });
